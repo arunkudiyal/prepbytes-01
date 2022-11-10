00a865d376a96769608f44ab21152765ff9cc288
arunkudiyal <arunkudiyal@icloud.com>
2022-11-10 20:53:02 +0530

    JS Data Types - number, string, boolean, null, undefined | Operators - Arithmetic & Comparitive

---
+++ JavaScript/main.js
@@ -0,0 +1,88 @@
+// JAVACRIPT
+// 1. JS is an intepreted lang -> executes line-by-line
+// 2. JS is helpful for making dynamic (on run-time) Front_End Applications using DOM & AJAX
+// 3. JS is a programming language (it will make decision for you), Its a complete OOPs
+
+// 1. How to print values in JS
+// console.log('Hello...')
+// console.warn('This is a warning..')
+// console.error('This is a warning..')
+
+// Data-Types, Variables and Constants
+// Data Types -> Nothing but different type of data that JS understand
+// 1. Primitive Data Types (PDTs)
+// i. number
+// variable1 = 1000
+// console.log(variable1 + 10)
+
+// Add two numbers
+// num1 = 100
+// num2 = 3
+// sum = num1 + num2
+// console.log(sum)
+
+// // ii. string
+// string1 = 'String - One'
+// string2 = 'String - Two'
+// string3 = 'String - Three'
+// console.log(string1, string2, string3)
+
+// // iii. boolean
+// console.log(true)
+// console.log(false)
+
+// // iv. null
+// let myVarible = 'This is a String'
+// console.log(myVarible)
+// myVarible = 100
+// console.log(myVarible)
+
+// console.log(myVarible)
+
+// // v. undefined
+// console.log(myVaribale2)
+
+// vi. Symbol
+
+
+// OPERATORS -> what operation do you wnat to perform on the data
+// 1. Arithmetic Operations
+// variable1 = 100
+// variable2 = 2
+
+// sum = variable1 + variable2
+// diff = variable1 - variable2
+// product = variable1 * variable2
+// // Ans is the quotient
+// division = variable1 / variable2
+
+// console.log(sum)
+// console.log(diff)
+// console.log(product)
+// console.log(division)
+
+// // Modulous -> Ans is the remainder
+// mod = variable1 % variable2
+// console.log(mod)
+
+// Comparitive Operators
+
+// CHECK THE DATA TYPE OF A VARIABLE - typeof
+// console.log(typeof(value1))
+// console.log(typeof("Hello"))
+// console.log(typeof(true))
+
+// INTERNAL_TYPECASTING
+value1 = 1
+value2 = true
+
+// console.log(value1 - value2)
+// console.log( value1 > value2 )
+// console.log( value1 < value2 )
+// console.log( value1 >= value2 )
+// console.log( value1 <= value2 )
+
+console.log(value1 == value2)   // This checks only the value of the 2 variable
+console.log(value1 === value2)  // This checks not the value BUT ALSO the type
+
+// 2. Non-Primitive Data Types (Non-PDTs)
