c714524919bf312fa6dd131f10ad42b748a72188
arunkudiyal <arunkudiyal@icloud.com>
2022-12-07 20:50:54 +0530

    GitHub Clone

---
+++ GitHub_Clone/main.js
@@ -0,0 +1,28 @@
+// The moment application statrts, i want to load all the data from the API
+// Reach to an API --> JS --> Class -> XMLHttpRequest
+const xhr = new XMLHttpRequest()
+const url = `https://api.github.com/users`
+// const url = `https://api.github.com/users/${}`
+console.log(`1st readyState value ${xhr.readyState}`)
+// READYSTATE -> 0
+
+// Step 1 - Open the portal of communication b/w the client and the server
+xhr.open('GET', url)
+// READYSTATE -> 1
+console.log(`2nd readyState value ${xhr.readyState}`)
+
+// Step2 - Perform actions on ready state change
+xhr.onreadystatechange = () => {
+    console.log(`3rd readyState value ${xhr.readyState}`)
+    if(xhr.readyState === 4 && xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText)
+        let output = ''
+        response.forEach( res => {
+            output += `<img style="margin: 10px;" src="${res.avatar_url}" />`
+        })
+        document.querySelector('#main-content').innerHTML = output
+    }
+}
+
+// Step 3 - Send the request
+xhr.send()
